83dbf5a0e05e5a3cf8fbc24b924b177c1270a27c
herreang000 <herreang000@warren.k12.in.us>
2024-08-06 11:59:11 -0400

    final change

--- src/assignment.java
+++ src/assignment.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class assignment {
+public class assignment {
+    public static void main(String[] args) {
+        byte byteValue = 40;
+        byte shortValue = 20;
+        int intValue;
+        intValue = 4;
+        long longTotal = 8000 - 5 * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+    }
 }
